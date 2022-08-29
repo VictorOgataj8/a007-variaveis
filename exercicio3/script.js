84adeeead305e0d7e116e0e45d591f4cd658d33e
VictorOgataj8 <victor.ogataj8@gmail.com>
2022-08-29 14:56:42 -0300

    exercicio 3 feito

---
+++ exercicio3/script.js
@@ -0,0 +1,13 @@
+let a = prompt("Qual e o valor de A")
+let b = prompt("Qual e o valor de B")
+let c
+console.log('o valor de a e', a)
+console.log('o valor de b e', b)
+
+c= a
+a= b
+b= c
+
+console.log("O novo valor de a é", a,)
+console.log("O novo valor de b é", b)
+console.log(c)
